160983b2de24573837a1802c667f3e5b0956e849
deiv44 <kede.leones.up@phinmaed.com>
2023-12-08 16:14:24 +0800

    2nd commit

--- KeilizonLeonesBlock3Quiz/app/src/main/java/com/example/keilizonleones_block3_quiz/MainActivity.kt
+++ KeilizonLeonesBlock3Quiz/app/src/main/java/com/example/keilizonleones_block3_quiz/MainActivity.kt
@@ -3,11 +3,25 @@ package com.example.keilizonleones_block3_quiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonOpenActivity: Button = findViewById(R.id.button)
+
+        buttonOpenActivity.setOnClickListener {
+
+            val intent = Intent(this, BMIcalcu::class.java)
+
+
+            startActivity(intent)
+
+        }
     }
 
     fun backbttn(view: View) {
